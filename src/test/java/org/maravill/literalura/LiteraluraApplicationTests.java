2a907420a6b08b6b805a60214c8124a6708256ea
Eduardo Maravilla <eduardomaravillag@gmail.com>
2025-05-21 18:53:04 -0600

    Create Project Literalura

---
+++ src/test/java/org/maravill/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maravill.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
